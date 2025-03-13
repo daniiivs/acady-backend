c49d738a40571f197cf2702d993d40fdef0105e1
Dani <danielvs90@gmail.com>
2025-03-13 18:18:29 +0000

    Inicialización del proyecto en Spring Boot

---
+++ src/main/java/org/springboot/acadybackend/AcadyBackendApplication.java
@@ -0,0 +1,13 @@
+package org.springboot.acadybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcadyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcadyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/springboot/acadybackend/AcadyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springboot.acadybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcadyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
